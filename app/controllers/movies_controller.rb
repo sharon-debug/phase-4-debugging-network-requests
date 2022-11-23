d396b05ba64a40b06a73d6036fa7bee6be5a26a9
sharon <sharon.chepkemoi@student.moringaschool.com>
2022-11-23 19:24:52 +0300

    code along

--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -1,8 +1,19 @@
 class MoviesController < ApplicationController
   
-  def index
+ def index
     movies = Movie.all
     render json: movies
   end
+  
+  def create
+    movie = Movi.create(movie_params)
+    render json: movie
+end
+ 
+private
+
+def movie_params
+  params.permit(:title, :year, :length, :director, :description, :poster_url, :category, :discount, :female_director)
+end
 
 end
